05f82b278e86a2cfd29bf3228cd1b1ff80427e61
Brandon Ticona <jbticona4@gmail.com>
2021-08-03 15:08:45 -0400

    change preloader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 import Home from './components/Home/Home';
 import About from './components/About/About';
 
-import Preloader from './components/Preloader/Preloader';
+// import Preloader from './components/Preloader/Preloader';
 import Services from './components/Services/Services';
 import Portfolio from './components/Portfolio/Portfolio';
 // import Testimonial from './components/Testimonial/Testimonial';
@@ -12,19 +12,19 @@ import Switcher from './components/Switcher/Switcher';
 import Footer from './components/Footer/Footer';
 
 function App() {
-  useEffect(() => {
-    // hide all sections except active
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-      if (!section.classList.contains('active')) {
-        section.classList.add('hide');
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   // hide all sections except active
+  //   const sections = document.querySelectorAll('.section');
+  //   sections.forEach(section => {
+  //     if (!section.classList.contains('active')) {
+  //       section.classList.add('hide');
+  //     }
+  //   });
+  // }, []);
 
   return (
     <>
-      <Preloader />
+      {/* <Preloader /> */}
       <Home />
       <About />
       <Portfolio />
